44809288f73a466b7d85a0ddbeb4275912b84a6c
Neyaz Morshid <niyazmorshid@gmail.com>
2024-12-20 19:23:24 +0600

    pagination added succssfully but a by product bug born

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 
+// ema-jhon
+// 233Morshid313
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_SecrectKey}:${process.env.DB_UserName}@cluster0.epj76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,17 +27,46 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
+// all products loading
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      // checking the query which is come from client side
+      // console.log('pagination query', req.query)
+
+  //-- to load dynamically data in the page:
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        const skip = page * size ;
+        const result = await productCollection.find().skip(skip).limit(size).toArray();
         res.send(result);
     })
 
+    // to count total number of products 
+    app.get('/productsCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count });
+    })
+
+
+    // psoting / loading orderd data 
+    app.post('/productByIds', async(req, res)=>{
+      const ids = req.body;
+      const IdsWithObjectId = ids.map(id=> new ObjectId(id));
+      const query ={
+        _id:{
+      // $in --> an advance operator of mongodb  
+          $in:IdsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray()
+      // console.log(ids);
+      res.send(result) ;
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
